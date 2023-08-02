54ccfb448bbdd51d2d9276c57e4e928021472cd1
Angela Sidhu <angela02sidhu@gmail.com>
2023-08-02 13:04:17 -0400

    Initial Commit

---
+++ melodai pages/melodai_pagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  melodai_pagesApp.swift
+//  melodai pages
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct melodai_pagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
